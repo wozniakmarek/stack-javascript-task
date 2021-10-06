87b7949208d53494fb2e58bb5e223338717ef770
BoskiMarecki <72198070+BoskiMarecki@users.noreply.github.com>
2021-10-06 22:33:07 +0200

    Done
    Tasks completed 7/7

--- stack.js
+++ stack.js
@@ -1,1 +1,32 @@
 // Your implementation goes here
+function Stack(...args) {
+    const arr = [...args]
+    this.size = arr.length;
+    this.push = function (arg) {
+        arr.push(arg)
+        this.size++;
+    }
+    this.isEmpty = function () {
+        if (arr.length > 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+    this.pop = function () {
+        if (arr.length > 0) {
+            this.size--;
+            return arr.pop();
+        } else {
+            return null
+        }
+    }
+    this.peek = function () {
+        return arr[arr.length - 1];
+    }
+    this.swap = function () {
+        [arr[arr.length - 1], arr[arr.length - 2]] = [arr[arr.length - 2], arr[arr.length - 1]]
+    }
+}
+
+module.exports = Stack;
